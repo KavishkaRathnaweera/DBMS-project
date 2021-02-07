11608c70ab1659757dc97eb1c18eb463398f1e48
theekshanamadumal <theekshana.18@cse.mrt.ac.lk>
2021-02-07 16:29:31 +0530

    employee iitialized

--- routes/employee.js
+++ routes/employee.js
@@ -6,4 +6,12 @@ const employeeController=require("../controllers/employeeController")
 
 router.get('/', employeeController.index);
 
+router.use("/applyLeave", employeeController.applyLeave);
+router.use("/attendance", employeeController.attendance);
+router.use("/employeeInfo", employeeController.employeeInfo);
+router.use("/leavesHistory", employeeController.leavesHistory);
+
+
+
+
 module.exports=router
